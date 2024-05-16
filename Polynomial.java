a5371ca6c2e42b84881cb373d38cc8c9a3275b8f
raifa-s <raifa.sarwar29@gmail.com>
2024-05-16 11:25:10 -0400

    added Polynomial.java

---
+++ Polynomial.java
@@ -0,0 +1,12 @@
+public class Polynomial {
+	double[] coefficients;
+
+	public Polynomial() {
+		coefficients = {0};
+	
+	public Polynomial(double[] a) {
+		coefficients = a
+
+	string add(Polynomial p) {
+		
+	
